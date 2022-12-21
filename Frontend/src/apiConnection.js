3768a3e85a4f74635378a95d892a33e83a5c93b8
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2022-12-20 22:42:57 -0500

    Update docker-compose and base url

--- Frontend/src/apiConnection.js
+++ Frontend/src/apiConnection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "/",
   headers: {
     "Content-type": "application/json",
     "Authorization":`Bearer ${localStorage.getItem("accessToken")}`,
