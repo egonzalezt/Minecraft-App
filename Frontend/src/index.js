467ca41d1bcd733408fab30817488f10a15bb851
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2023-03-12 14:58:59 -0500

    Add elastic APM

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { init as initApm } from '@elastic/apm-rum'
+
+const apm = initApm({
+
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: 'Arequipet Front',
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'https://apm.vasitos.software',
+
+  // Set service version (required for sourcemap feature)
+  serviceVersion: '1.0.0'
+
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
